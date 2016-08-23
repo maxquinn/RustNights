4252b875f14e35d35f7c211ba93e1924e096e472
Max <max.t.quinn@gmail.com>
2016-08-24 10:54:24 +1200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace RustNights
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.DayTimeButton = new System.Windows.Forms.Button();
+            this.NightTimeButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DefaultButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // DayTimeButton
+            // 
+            this.DayTimeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.DayTimeButton.BackColor = System.Drawing.Color.Gold;
+            this.DayTimeButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.DayTimeButton.Font = new System.Drawing.Font("Calibri", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DayTimeButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.DayTimeButton.Location = new System.Drawing.Point(82, 83);
+            this.DayTimeButton.Name = "DayTimeButton";
+            this.DayTimeButton.Size = new System.Drawing.Size(134, 63);
+            this.DayTimeButton.TabIndex = 0;
+            this.DayTimeButton.Text = "Day";
+            this.DayTimeButton.UseVisualStyleBackColor = false;
+            this.DayTimeButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // NightTimeButton
+            // 
+            this.NightTimeButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.NightTimeButton.BackColor = System.Drawing.Color.Navy;
+            this.NightTimeButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.NightTimeButton.Font = new System.Drawing.Font("Calibri", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NightTimeButton.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.NightTimeButton.Location = new System.Drawing.Point(82, 83);
+            this.NightTimeButton.Name = "NightTimeButton";
+            this.NightTimeButton.Size = new System.Drawing.Size(134, 63);
+            this.NightTimeButton.TabIndex = 1;
+            this.NightTimeButton.Text = "Night";
+            this.NightTimeButton.UseVisualStyleBackColor = false;
+            this.NightTimeButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(187, 239);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Created by DMT©";
+            // 
+            // DefaultButton
+            // 
+            this.DefaultButton.Location = new System.Drawing.Point(12, 229);
+            this.DefaultButton.Name = "DefaultButton";
+            this.DefaultButton.Size = new System.Drawing.Size(89, 23);
+            this.DefaultButton.TabIndex = 3;
+            this.DefaultButton.Text = "Default Gamma";
+            this.DefaultButton.UseVisualStyleBackColor = true;
+            this.DefaultButton.Click += new System.EventHandler(this.DefaultButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.DefaultButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NightTimeButton);
+            this.Controls.Add(this.DayTimeButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Rust Nights";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button DayTimeButton;
+        private System.Windows.Forms.Button NightTimeButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button DefaultButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace RustNights
+{
+    public partial class Form1 : Form
+    {
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+        public struct RAMP
+        {
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 256)]
+            public UInt16[] Red;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 256)]
+            public UInt16[] Green;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 256)]
+            public UInt16[] Blue;
+        }
+
+        private static RAMP userDefault = new RAMP();
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetDC(IntPtr hWnd);
+
+        [DllImport("gdi32.dll")]
+        public static extern bool SetDeviceGammaRamp(IntPtr hDC, ref RAMP lpRamp);
+
+        [DllImport("gdi32.dll")]
+        public static extern bool GetDeviceGammaRamp(IntPtr hDC, ref RAMP lpRamp);
+
+        public static void SetGamma(int gamma)
+        {
+            if (gamma <= 256 && gamma >= 1)
+            {
+                RAMP ramp = new RAMP();
+                ramp.Red = new ushort[256];
+                ramp.Green = new ushort[256];
+                ramp.Blue = new ushort[256];
+                for (int i = 1; i < 256; i++)
+                {
+                    int iArrayValue = i * (gamma + 128);
+
+                    if (iArrayValue > 65535)
+                        iArrayValue = 65535;
+                    ramp.Red[i] = ramp.Blue[i] = ramp.Green[i] = (ushort)iArrayValue;
+                }
+                SetDeviceGammaRamp(GetDC(IntPtr.Zero), ref ramp);
+            }
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            GetDeviceGammaRamp(GetDC(IntPtr.Zero), ref userDefault);
+            this.FormClosing += this.Form1_FormClosing;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SetDeviceGammaRamp(GetDC(IntPtr.Zero), ref userDefault);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.DayTimeButton.Visible = false;
+            this.NightTimeButton.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SetGamma(255);
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.DayTimeButton.Visible = true;
+            this.NightTimeButton.Visible = false;
+        }
+
+        private void Form1_FormClosing(Object sender, FormClosingEventArgs e)
+        {
+                SetDeviceGammaRamp(GetDC(IntPtr.Zero), ref userDefault);
+        }
+
+        private void DefaultButton_Click(object sender, EventArgs e)
+        {
+            SetGamma(128);
+        }
+    }
+}
